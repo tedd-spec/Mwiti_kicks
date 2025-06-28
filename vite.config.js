f3451f7c466f810e88e2f329e3e3ddf72ed480df
tedd-spec <gethappywithted@gmail.com>
2025-06-28 14:50:52 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    publicDir: 'public', // ensures _redirects is copied
+    build: {
+        outDir: 'dist', // Netlify expects this
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
